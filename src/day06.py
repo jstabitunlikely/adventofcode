abf32e17b3c1f4f22d78655d84c96632fd79217b
Gergo Gacs <gacsgergo@gmail.com>
2024-12-29 15:33:04 +0800

    (refactor) day06 again

--- src/day06.py
+++ src/day06.py
@@ -26,83 +26,96 @@ DIR_MAP = {
 }
 
 
-def parse_input(example=False):
+def parse_input(example: bool):
     data = EXAMPLE if example else inputfetcher.fetch_input('2024', '6')
     return data.split()
 
 
-def turn_right(dir: str) -> str:
-    dir_cnt = DIRECTION.index(dir)
-    return DIRECTION[(dir_cnt + 1) % 4]
+def turn_right(dir: int) -> int:
+    return (dir + 1) % 4
 
 
-def turn_left(dir: str) -> str:
-    dir_cnt = DIRECTION.index(dir)
-    return DIRECTION[(dir_cnt + 3) % 4]
-
-
-def step(dir: str,
+def step(dir: int,
          px: int,
          py: int) -> tuple[int, int]:
-    px_next = px + DIR_MAP[dir][0]
-    py_next = py + DIR_MAP[dir][1]
+    px_next = px + DIR_MAP[DIRECTION[dir]][0]
+    py_next = py + DIR_MAP[DIRECTION[dir]][1]
     return (px_next, py_next)
 
 
-def is_obstacle_ahead(dir: str,
+def is_obstacle_ahead(dir: int,
                       px: int,
                       py: int,
                       obstacles: list[tuple[int, int]]) -> bool:
     return step(dir, px, py) in obstacles
 
 
-def walk(guard: tuple[int, int, str],
-         obstacles: list[tuple],
-         lab_map: list[list]) -> int:
-    path = []
-    loops = 0
-    px, py, dir = guard
-    while True:
-        path.append((px, py, dir))
-        if is_obstacle_ahead(dir, px, py, obstacles):
-            dir = turn_right(dir)
-        else:
-            # px_alt, py_alt, dir_alt = px, py, turn_right(dir)
-            # while True:
-            #     if (px_alt, py_alt, dir_alt) in path:
-            #         loops += 1
-            #         break
-            #     elif is_obstacle_ahead(dir_alt, px_alt, py_alt, obstacles):
-            #         dir_alt = turn_right(dir_alt)
-            #     else:
-            #         px_alt, py_alt = step(dir_alt, px_alt, py_alt)
-            #         try:
-            #             lab_map[px_alt][py_alt]
-            #         except IndexError:
-            #             break
-            px, py = step(dir, px, py)
-            try:
-                lab_map[px][py]
-            except IndexError:
-                unique_path_len = len(set([(p[0], p[1]) for p in path]))
-                return unique_path_len, loops
-
-
-def solve_1_2(lab_map):
-    guard = ()
+def find_obstacles(lab_map: list[list]) -> list[tuple[int, int]]:
     obstacles = []
     for x, row in enumerate(lab_map):
         for y, e in enumerate(row):
-            if e in ["^", ">", "v", "<"]:
-                guard = (x, y, e)
-            elif e == '#':
+            if e == '#':
                 obstacles.append((x, y))
-    return walk(guard, obstacles, lab_map)
+    return obstacles
+
+
+def find_guard(lab_map: list[list]):
+    for x, row in enumerate(lab_map):
+        for y, e in enumerate(row):
+            if e in ["^", ">", "v", "<"]:
+                return (DIRECTION.index(e), x, y)
+
+
+def walk(guard: tuple[int, int, int],
+         obstacles: list[tuple[int, int]],
+         lab_map: list[list]) -> list[tuple[int, int, int]]:
+    path = []
+    dir, px, py = guard
+    while True:
+        path.append((dir, px, py))
+        # Check if we're running in circles (for Part 2 only)
+        if len(path) > 2 and path[-1] == path[0]:
+            break
+        if is_obstacle_ahead(dir, px, py, obstacles):
+            dir = turn_right(dir)
+        else:
+            px, py = step(dir, px, py)
+            # Check if we've walked off the map
+            try:
+                lab_map[px][py]
+            except IndexError:
+                break
+    return path
+
+
+def solve_1(lab_map: list[list]) -> int:
+    obstacles = find_obstacles(lab_map)
+    guard = find_guard(lab_map)
+    path = walk(guard, obstacles, lab_map)
+    unique_path_len = len(set([(p[1], p[2]) for p in path]))
+    return unique_path_len
+
+
+def solve_2(lab_map: list[list]) -> int:
+    obstacles = find_obstacles(lab_map)
+    guard = find_guard(lab_map)
+    path = walk(guard, obstacles, lab_map)
+    loops = 0
+    # placeholder for the potential obstacle, so we don't have to re-allocate the array each time
+    obstacles.append((-1, -1))
+    for dir, px, py in path[1:-1]:
+        if not is_obstacle_ahead(dir, px, py, obstacles):
+            obstacles[-1] = step(dir, px, py)
+            new_path = walk((dir, px, py), obstacles, lab_map)
+            if new_path[-1] == new_path[0]:
+                loops += 1
+    return loops
 
 
 if __name__ == "__main__":
     example = "--example" in sys.argv
     lab_map = parse_input(example=example)
-    result_1, result_2 = solve_1_2(lab_map)
+    result_1 = solve_1(lab_map)
     print(f'Result 1: {result_1}')
+    result_2 = solve_2(lab_map)
     print(f'Result 2: {result_2}')
