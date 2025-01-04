8a9d5462ff56847d7045975922184a56530e5951
Gergo Gacs <gacsgergo@gmail.com>
2025-01-04 19:14:46 +0800

    feat(day12): add solution part 2

--- src/day12.py
+++ src/day12.py
@@ -19,17 +19,21 @@ MIIISIJEEE
 MMMISSJEEE\
 """
 
-EXAMPLE = """\
-AAAA
-BBCD
-BBCC
-EEEC\
-"""
-
 
 def parse_input(example: bool) -> list[list[str]]:
     data = EXAMPLE if example else inputfetcher.fetch_input('2024', '12')
     return parse_matrix2d(data, str)
+
+
+def get_sides(p: Coordinate,
+              garden: list[list[str]]) -> list[bool]:
+    sides = []
+    for n in p.get_neighbor_coordinates():
+        if is_on_map(n, garden):
+            sides.append(garden[n.x][n.y] != garden[p.x][p.y])
+        else:
+            sides.append(True)
+    return sides
 
 
 def solve_1_2(garden: list[list[str]],
@@ -40,17 +44,24 @@ def solve_1_2(garden: list[list[str]],
             p = Coordinate(i, j)
             G.add_node(p)
             neighbors = [np for np in p.get_neighbor_coordinates() if is_on_map(np, garden)]
-            G.add_edges_from([(p, n) for n in neighbors if plot == garden[n.x][n.y]])
+            for n in neighbors:
+                if plot == garden[n.x][n.y]:
+                    d = 0
+                    if bulk_discount:
+                        # REVISIT: I feel like this spoils the elegance of the graph approach.
+                        sides_p = get_sides(p, garden)
+                        sides_n = get_sides(n, garden)
+                        d = sum([a & b for (a, b) in zip(sides_p, sides_n)])
+                    G.add_edge(p, n, discount=d)
     regions = list(nx.connected_components(G))
     price = 0
     for r in regions:
         area = len(r)
-        if bulk_discount:
-            # TODO: handle bulk discount
-            sides = [4-d for _, d in G.degree(r)]
-        else:
-            sides = [4-d for _, d in G.degree(r)]
+        sides = [4-d for _, d in G.degree(r)]
         perimeter = sum(sides)
+        if bulk_discount:
+            discounts = [d["discount"] for (_, _, d) in G.edges(r, data=True)]
+            perimeter -= sum(discounts)
         price += area * perimeter
     return price
 
