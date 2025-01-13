bb489d815c30ab471dcc1a92d50bc03a75db2eca
Gergo Gacs <gacsgergo@gmail.com>
2025-01-13 15:59:57 +0800

    refactor(day11): apply rules in separate function

--- src/day11.py
+++ src/day11.py
@@ -12,35 +12,32 @@ def parse_input(example: bool) -> list[list[str]]:
     return [int(n) for n in data.split()]
 
 
+def evolve(stone: int):
+    if not stone:
+        return [1]
+    if not (dc := math.floor(math.log10(stone)) + 1) % 2:
+        return [stone % (10**(dc/2)), stone // (10**(dc/2))]
+    return [stone * 2024]
+
+
 def blink(stone: int,
           blinks: int,
           memo: dict) -> list[int]:
-    if (stone, blinks) in memo:
-        return memo[(stone, blinks)]
     if not blinks:
         return 0
-    # Rule #1
-    if stone == 0:
-        new_stones = blink(1, blinks-1, memo)
-    # Rule #2
-    # Note: Zero and negative numbers are not handled
-    elif not (dc := int(math.log10(stone)) + 1) % 2:
-        new_stones = 1
-        top = int(stone / (10**(dc/2)))
-        bottom = int(stone % (10**(dc/2)))
-        new_stones += blink(top, blinks-1, memo)
-        new_stones += blink(bottom, blinks-1, memo)
-    # Rules #3
-    else:
-        new_stones = blink(stone*2024, blinks-1, memo)
-    memo[(stone, blinks)] = new_stones
-    return new_stones
+    if (m := memo.get((stone, blinks), None)) is not None:
+        return m
+    evolved_stones = evolve(stone)
+    num_stones = len(evolved_stones) - 1
+    num_stones += sum([blink(s, blinks-1, memo) for s in evolved_stones])
+    memo[(stone, blinks)] = num_stones
+    return num_stones
 
 
 def solve_1_2(stones: list[int],
               blinks: int = 1) -> int:
-    new_stones = [blink(s, blinks, {}) for s in stones]
-    return len(stones) + sum(new_stones)
+    memo = {}
+    return len(stones) + sum([blink(s, blinks, memo) for s in stones])
 
 
 if __name__ == "__main__":
