f90dd5ae1fcb07c3e7f7f3ec108e0b2627fa03ff
Gergo Gacs <gacsgergo@gmail.com>
2025-01-24 10:33:27 +0800

    feat(day22): add a fast version of solution to part 1

--- src/day22.py
+++ src/day22.py
@@ -37,7 +37,7 @@ def parse_input(example: bool) -> list[int]:
     return [int(i) for i in data.split('\n')]
 
 
-def solve_1(numbers: list[int]) -> int:
+def solve_1_cpu(numbers: list[int]) -> int:
     regbank = CSRegbank({
         'A': 6,
         'B': 0,     # input number
@@ -59,10 +59,41 @@ def solve_1(numbers: list[int]) -> int:
     return secret_sum
 
 
+def solve_1(numbers: list[int]) -> int:
+    N = 2000
+    MODMASK = (1 << 24) - 1  # Create a bitmask with N set bits
+    secrets = [[0 for _ in range(N)] for _ in range(len(numbers))]
+    for i, number in enumerate(numbers):
+        secret = number
+        for j in range(N):
+            # 1.
+            # Calculate the result of multiplying the secret number by 64.
+            # Then, mix this result into the secret number.
+            secret ^= (secret << 6)
+            # Finally, prune the secret number.
+            secret &= MODMASK
+            # 2.
+            # Calculate the result of dividing the secret number by 32. Round the result down to the nearest integer.
+            # Then, mix this result into the secret number.
+            secret ^= (secret >> 5)
+            # Finally, prune the secret number.
+            secret &= MODMASK
+            # 3.
+            # Calculate the result of multiplying the secret number by 2048.
+            # Then, mix this result into the secret number.
+            secret ^= (secret << 11)
+            # Finally, prune the secret number.
+            secret &= MODMASK
+            secrets[i][j] = secret
+    return sum([s[-1] for s in secrets])
+
+
 if __name__ == "__main__":
     use_example = "--example" in sys.argv
     numbers = parse_input(use_example)
     result_1 = solve_1(numbers)
     if use_example:
         assert result_1 == 37327623, result_1
+        result_1_cpu = solve_1_cpu(numbers)
+        assert result_1_cpu == result_1, result_1_cpu
     print(f'Result 1: {result_1}')
