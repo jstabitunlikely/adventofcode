3cd9848c70e5d3f8593b2aa9ddf34cda93095820
Gergo Gacs <gacsgergo@gmail.com>
2025-12-02 13:57:20 +0100

    25d02: parse input

---
+++ src/2025/y25d02.py
@@ -0,0 +1,36 @@
+from Day import Day
+
+
+class y25d02(Day):
+
+    def __init__(self,
+                 auto_fetch: bool = True,
+                 auto_parse: bool = True) -> None:
+        super().__init__(year='2025', day='02', auto_fetch=auto_fetch, auto_parse=auto_parse)
+
+    def parse_puzzle(self) -> None:
+        super().parse_puzzle()
+        ranges = []
+        for range_ in self.puzzle_raw.split(','):
+            min, max = range_.split('-')
+            ranges.append((int(min), int(max)))
+        self.puzzle = ranges
+
+    def solve_part_1(self) -> int:
+        answer = 0
+        return answer
+
+    def solve_part_2(self) -> int:
+        return 0
+
+
+def main() -> dict[str, str]:  # pragma: no cover
+    today = y25d02()
+    today.solve()
+    return today.answer
+
+
+if __name__ == "__main__":  # pragma: no cover
+    answer = main()
+    print(f'Part 1: {answer['part_1']}')
+    print(f'Part 2: {answer['part_2']}')
