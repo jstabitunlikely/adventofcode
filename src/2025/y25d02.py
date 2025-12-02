d30cccefb7e855552e2475d6dfc7a8242d5ba04c
Gergo Gacs <gacsgergo@gmail.com>
2025-12-02 19:52:54 +0100

    feat(y25d02): add solution 2

--- src/2025/y25d02.py
+++ src/2025/y25d02.py
@@ -1,3 +1,4 @@
+import re
 from Day import Day
 
 
@@ -7,6 +8,8 @@ class y25d02(Day):
                  auto_fetch: bool = True,
                  auto_parse: bool = True) -> None:
         super().__init__(year='2025', day='02', auto_fetch=auto_fetch, auto_parse=auto_parse)
+        self.INVALID_ID_RE_1 = re.compile(r'^([0-9]+)\1$')
+        self.INVALID_ID_RE_2 = re.compile(r'^([0-9]+)\1+$')
 
     def parse_puzzle(self) -> None:
         super().parse_puzzle()
@@ -16,18 +19,25 @@ class y25d02(Day):
             ranges.append((int(min), int(max)))
         self.puzzle = ranges
 
+    # TODO Merge solution 1-2 on a rainy day
+
     def solve_part_1(self) -> int:
         answer = 0
         for r in self.puzzle:
             for i in range(r[0], r[1]+1):
-                idx = len(str(i)) // 2
                 i_str = str(i)
-                if (i_str[:idx] == i_str[idx:]):
+                if self.INVALID_ID_RE_1.search(i_str):
                     answer += i
         return answer
 
     def solve_part_2(self) -> int:
-        return 0
+        answer = 0
+        for r in self.puzzle:
+            for i in range(r[0], r[1]+1):
+                i_str = str(i)
+                if self.INVALID_ID_RE_2.search(i_str):
+                    answer += i
+        return answer
 
 
 def main() -> dict[str, str]:  # pragma: no cover
