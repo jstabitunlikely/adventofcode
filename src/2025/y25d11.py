a1a10081824c03e05d232be76defa30b99eb5247
Gergo Gacs <gacsgergo@gmail.com>
2025-12-17 15:16:49 +0100

    feat(y25d11): add solution 2

--- src/2025/y25d11.py
+++ src/2025/y25d11.py
@@ -22,7 +22,7 @@ class y25d11(Day):
         for dev_id, dev_conn in self.puzzle:
             for o in dev_conn:
                 network.add_edge(dev_id, o)
-        nof_paths = len(list(nx.all_simple_paths(network, 'you', 'out')))
+        nof_paths = self.count_paths(network, 'you', 'out')
         return nof_paths
 
     def solve_part_2(self) -> int:
@@ -34,34 +34,18 @@ class y25d11(Day):
         self.do_topological_ordering(network)
         # self.visualize(network)
 
-        try:
-            print("SVR-DAC-FFT-OUT")
-            print("\tSVR-DAC")
-            nof_paths_svr_dac = len(list(nx.shortest_simple_paths(network, 'svr', 'dac')))
-            print("\tDAC-FFT")
-            nof_paths_dac_fft = len(list(nx.shortest_simple_paths(network, 'dac', 'fft')))
-            print("\tFFT-OUT")
-            nof_paths_fft_out = len(list(nx.shortest_simple_paths(network, 'fft', 'out')))
-        except nx.NetworkXNoPath:
-            nof_paths_svr_dac = 0
-            nof_paths_dac_fft = 0
-            nof_paths_fft_out = 0
-
-        try:
-            print("SVR-FFT-DAC-OUT")
-            print("\tSVR-FFT")
-            nof_paths_svr_fft = len(list(nx.shortest_simple_paths(network, 'svr', 'fft')))
-            print("\tFFT-DAC")
-            nof_paths_fft_dac = len(list(nx.shortest_simple_paths(network, 'fft', 'dac')))
-            print("\tDAC-OUT")
-            nof_paths_dac_out = len(list(nx.shortest_simple_paths(network, 'dac', 'out')))
-        except nx.NetworkXNoPath:
-            nof_paths_svr_fft = 0
-            nof_paths_fft_dac = 0
-            nof_paths_dac_out = 0
-
+        # Option 1
+        nof_paths_svr_dac = self.count_paths(network, 'svr', 'dac')
+        nof_paths_dac_fft = self.count_paths(network, 'dac', 'fft')
+        nof_paths_fft_out = self.count_paths(network, 'fft', 'out')
         nof_paths_svr_dac_fft_out = nof_paths_svr_dac * nof_paths_dac_fft * nof_paths_fft_out
+
+        # Option 2
+        nof_paths_svr_fft = self.count_paths(network, 'svr', 'fft')
+        nof_paths_fft_dac = self.count_paths(network, 'fft', 'dac')
+        nof_paths_dac_out = self.count_paths(network, 'dac', 'out')
         nof_paths_svr_fft_dac_out = nof_paths_svr_fft * nof_paths_fft_dac * nof_paths_dac_out
+
         nof_paths_total = nof_paths_svr_dac_fft_out + nof_paths_svr_fft_dac_out
         return nof_paths_total
 
@@ -83,6 +67,17 @@ class y25d11(Day):
         nx.draw_networkx_labels(G=dag, pos=pos, ax=ax, font_size=8, labels=node_labels)
         dag.nodes()
         plt.show()
+
+    def count_paths(self, graph: nx.DiGraph, source: str, target: str) -> int:
+        topo_order = list(nx.topological_sort(graph))
+        paths = {node: 0 for node in graph.nodes()}
+        paths[source] = 1
+        start_idx = topo_order.index(source)
+        for u in topo_order[start_idx:]:
+            if paths[u] > 0:
+                for v in graph.successors(u):
+                    paths[v] += paths[u]
+        return paths[target]
 
 
 def main() -> dict[str, int]:  # pragma: no cover
